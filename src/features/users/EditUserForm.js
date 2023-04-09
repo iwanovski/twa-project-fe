423727e1ba3a03c9d4129dd150e5ac887fc8910d
Ivan Havran <xhavran2@fi.muni.cz>
2023-04-09 21:37:42 +0200

    Add user editForm and newForm

---
+++ src/features/users/EditUserForm.js
@@ -0,0 +1,195 @@
+import { useState, useEffect } from "react"
+import { useUpdateUserMutation, useDeleteUserMutation } from "./usersApiSlice"
+import { useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons"
+
+const USER_REGEX = /^[A-z0-9]{3,20}$/
+const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const FULL_NAME_REGEX = /^[A-z0-9 ]{3,50}$/
+
+const EditUserForm = ({ user }) => {
+
+    const [updateUser, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useUpdateUserMutation()
+
+    const [deleteUser, {
+        isSuccess: isDelSuccess,
+        isError: isDelError,
+        error: delerror
+    }] = useDeleteUserMutation()
+
+    const navigate = useNavigate()
+
+    const [username, setUsername] = useState(user.username)
+    const [validUsername, setValidUsername] = useState(false)
+    const [password, setPassword] = useState('')
+    const [validPassword, setValidPassword] = useState(false)
+    const [email, setEmail] = useState(user.email)
+    const [validEmail, setValidEmail] = useState(false)
+    const [fullName, setFullName] = useState(user.fullName)
+    const [validFullName, setValidFullName] = useState(false)
+    const [active, setActive] = useState(user.active)
+
+    useEffect(() => {
+        setValidUsername(USER_REGEX.test(username))
+    }, [username])
+
+    useEffect(() => {
+        setValidPassword(PWD_REGEX.test(password))
+    }, [password])
+
+    useEffect(() => {
+        setValidEmail(EMAIL_REGEX.test(email))
+    }, [email])
+
+    useEffect(() => {
+        setValidFullName(FULL_NAME_REGEX.test(fullName))
+    }, [fullName])
+
+    useEffect(() => {
+        console.log(isSuccess)
+        if (isSuccess || isDelSuccess) {
+            setUsername('')
+            setPassword('')
+            setEmail('')
+            setFullName('')
+            navigate('/home/users')
+        }
+
+    }, [isSuccess, isDelSuccess, navigate])
+
+    const onUsernameChanged = e => setUsername(e.target.value)
+    const onPasswordChanged = e => setPassword(e.target.value)
+    const onEmailChanged = e => setEmail(e.target.value)
+    const onFullNameChanged = e => setFullName(e.target.value)
+
+    const onActiveChanged = () => setActive(prev => !prev)
+
+    const roles = []
+
+    const onSaveUserClicked = async (e) => {
+        if (password) {
+            await updateUser({ id: user.id, username, password, email, fullName, active, roles })
+        } else {
+            await updateUser({ id: user.id, username, email, fullName, active, roles })
+        }
+    }
+
+    const onDeleteUserClicked = async () => {
+        await deleteUser({ id: user.id })
+    }
+
+    let canSave
+    if (password) {
+        canSave = [validUsername, validPassword, validEmail, validFullName].every(Boolean) && !isLoading
+    } else {
+        canSave = [validUsername, validEmail, validFullName].every(Boolean) && !isLoading
+    }
+
+    const errClass = (isError || isDelError) ? "errmsg" : "offscreen"
+    const validUserClass = !validUsername ? 'form__input--incomplete' : ''
+    const validPwdClass = password && !validPassword ? 'form__input--incomplete' : ''
+    const validEmailClass = !validEmail ? 'form__input--incomplete' : ''
+    const validFullNameClass = !validFullName ? 'form__input--incomplete' : ''
+
+    const errContent = (error?.data?.message || delerror?.data?.message) ?? ''
+
+
+    const content = (
+        <>
+            <p className={errClass}>{errContent}</p>
+
+            <form className="form" onSubmit={e => e.preventDefault()}>
+                <div className="form__title-row">
+                    <h2>Edit User</h2>
+                    <div className="form__action-buttons">
+                        <button
+                            className="icon-button"
+                            title="Save"
+                            onClick={onSaveUserClicked}
+                            disabled={!canSave}
+                        >
+                            <FontAwesomeIcon icon={faSave} />
+                        </button>
+                        <button
+                            className="icon-button"
+                            title="Delete"
+                            onClick={onDeleteUserClicked}
+                        >
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        </button>
+                    </div>
+                </div>
+
+                <label className="form__label" htmlFor="username">
+                    Username: <span className="nowrap">[3-20 letters]</span></label>
+                <input
+                    className={`form__input ${validUserClass}`}
+                    id="username"
+                    name="username"
+                    type="text"
+                    autoComplete="off"
+                    value={username}
+                    onChange={onUsernameChanged}
+                />
+
+                <label className="form__label" htmlFor="password">
+                    Password: <span className="nowrap">[empty = no change]</span> <span className="nowrap">[4-12 chars incl. !@#$%]</span></label>
+                <input
+                    className={`form__input ${validPwdClass}`}
+                    id="password"
+                    name="password"
+                    type="password"
+                    value={password}
+                    onChange={onPasswordChanged}
+                />
+
+                <label className="form__label" htmlFor="email">
+                    Email: <span className="nowrap">[Valid email address]</span></label>
+                <input
+                    className={`form__input ${validEmailClass}`}
+                    id="email"
+                    name="email"
+                    type="text"
+                    autoComplete="off"
+                    value={email}
+                    onChange={onEmailChanged}
+                />
+
+                <label className="form__label" htmlFor="fullName">
+                    FullName: <span className="nowrap">[Full name of user]</span></label>
+                <input
+                    className={`form__input ${validFullNameClass}`}
+                    id="fullName"
+                    name="fullName"
+                    type="text"
+                    autoComplete="off"
+                    value={fullName}
+                    onChange={onFullNameChanged}
+                />
+
+                <label className="form__label form__checkbox-container" htmlFor="user-active">
+                    ACTIVE:
+                    <input
+                        className="form__checkbox"
+                        id="user-active"
+                        name="user-active"
+                        type="checkbox"
+                        checked={active}
+                        onChange={onActiveChanged}
+                    />
+                </label>
+
+            </form>
+        </>
+    )
+
+    return content
+}
+export default EditUserForm
--- src/features/users/usersApiSlice.js
+++ src/features/users/usersApiSlice.js
@@ -12,7 +12,6 @@ export const usersApiSlice = apiSlice.injectEndpoints({
             validateStatus: (response, result) => {
                 return response.status === 200 && !result.isError
             },
-            keepUnusedDataFor: 5,
             // Transform response from mongo query
             transformResponse: responseData => {
                 const loadedUsers = responseData.map(user => {
@@ -30,11 +29,48 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 } else return [{ type: 'User', id: 'LIST' }]
             }
         }),
+        addNewUser: builder.mutation({
+            query: initialUserData => ({
+                url: '/users',
+                method: 'POST',
+                body: {
+                    ...initialUserData,
+                }
+            }),
+            invalidatesTags: [
+                { type: 'User', id: "LIST" }
+            ]
+        }),
+        updateUser: builder.mutation({
+            query: initialUserData => ({
+                url: '/users',
+                method: 'PATCH',
+                body: {
+                    ...initialUserData,
+                }
+            }),
+            invalidatesTags: (result, error, arg) => [
+                { type: 'User', id: arg.id }
+            ]
+        }),
+        deleteUser: builder.mutation({
+            query: ({ id }) => ({
+                url: `/users`,
+                method: 'DELETE',
+                body: { id }
+            }),
+            invalidatesTags: (result, error, arg) => [
+                { type: 'User', id: arg.id }
+            ]
+        }),
     }),
 })
 // Automatically generated hook
 export const {
     useGetUsersQuery,
+    useAddNewUserMutation,
+    useUpdateUserMutation,
+    useDeleteUserMutation,
 } = usersApiSlice
 
 // returns the query result object
